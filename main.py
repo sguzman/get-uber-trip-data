17a420ccaff947bfe3a0708bf783e550ffe92e7b
Salvador Guzman <guzmansalv@gmail.com>
2019-11-05 11:52:56 -0800

    get endpoint for weekly earnings

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import requests
 import json
-from typing import IO
+from typing import IO, Dict
+
+url: str = 'https://partners.uber.com/p3/payments/api/fetchWeeklyEarning'
 
 
 def get_cookie() -> str:
@@ -11,9 +13,23 @@ def get_cookie() -> str:
     return json_obj["cookie"]
 
 
+def get_headers() -> Dict[str, str]:
+    return {
+        'x-csrf-token': 'x',
+        'cookie': get_cookie()
+    }
+
+
+def get_week(n: int) -> str:
+    resp = requests.post(url, json={'weekOffset': str(n)}, headers=get_headers())
+    json_str: str = resp.content
+    return json_str
+
+
 def main() -> None:
     print('Hello world')
     print(get_cookie())
+    print(get_week(1))
 
 
 if __name__ == '__main__':
