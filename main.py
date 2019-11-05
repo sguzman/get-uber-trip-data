b4af6e068451055948d70158d545653c51720ced
Salvador Guzman <guzmansalv@gmail.com>
2019-11-05 01:01:15 -0800

    get cookie

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import requests
 import json
+from typing import IO
+
+
+def get_cookie() -> str:
+    json_file: IO = open('./cookie.json', 'r')
+    json_str: str = json_file.read()
+    json_obj: json = json.loads(json_str)
+
+    return json_obj["cookie"]
 
 
 def main() -> None:
     print('Hello world')
+    print(get_cookie())
 
 
 if __name__ == '__main__':
