236adeda8202e89882070594889d9e53a82667c7
Salvador Guzman <guzmansalv@gmail.com>
2019-11-05 20:49:24 -0800

    working pay statement scraper

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import csv
 import requests
 import json
-from typing import IO, Dict
+from typing import IO, Dict, List
 
-url: str = 'https://partners.uber.com/p3/payments/api/fetchWeeklyEarning'
+url: str = 'https://partners.uber.com/p3/payments/api/fetchPayStatementsPaginated'
 
 
 def get_cookie() -> str:
@@ -16,20 +17,57 @@ def get_cookie() -> str:
 def get_headers() -> Dict[str, str]:
     return {
         'x-csrf-token': 'x',
+        'content-type': 'application/json',
         'cookie': get_cookie()
     }
 
 
-def get_week(n: int) -> str:
-    resp = requests.post(url, json={'weekOffset': str(n)}, headers=get_headers())
-    json_str: str = resp.content
-    return json_str
+def get_statement_page(offset: int) -> json:
+    data = {
+        "pageIndex": offset,
+        "pageSize": 100,
+        "pagination": {
+            "hasMoreData": True,
+            "nextCursor": "1539716406307",
+            "pageNumber": 3,
+            "totalPages": 4,
+            "cursors": ["1571466758406", "1541255383715", "1539716406307"]}
+    }
+    resp = requests.post(url, data=json.dumps(data), headers=get_headers())
+    json_str: str = resp.text
+    json_obj: json = json.loads(json_str)
+
+    return json_obj
+
+
+def get_statement_uuids(offset: int) -> List[str]:
+    page: json = get_statement_page(offset)
+    stmts = page['data']['payStatementsPaginatedEA']['statements']
+
+    uuids: List[str] = []
+    for stm in stmts:
+        uuids.append(stm['uuid'])
+
+    return uuids
+
+
+def get_all_statement_uuid() -> List[str]:
+    uuids: List[str] = []
+    i = 1
+    while True:
+        stmts: List[str] = get_statement_uuids(i)
+        print(i, len(stmts), stmts)
+
+        if len(stmts) == 0:
+            break
+        uuids.extend(stmts)
+        i += 1
+
+    return uuids
 
 
 def main() -> None:
-    print('Hello world')
-    print(get_cookie())
-    print(get_week(1))
+    print(get_all_statement_uuid())
 
 
 if __name__ == '__main__':
