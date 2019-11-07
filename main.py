e7bc51165a81e7d175967a64e7a9f1419a225f7f
Salvador Guzman <guzmansalv@gmail.com>
2019-11-06 16:18:01 -0800

    done i think

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
-import requests
+import atexit
 import json
-from typing import IO, Dict, List
+from datetime import datetime
+
+import psycopg2
+import requests
 import queue
 import threading
+from typing import IO, Dict, List, Optional
 
 statement_url: str = 'https://partners.uber.com/p3/payments/api/fetchPayStatementsPaginated'
 
 stmt_queue = queue.Queue()
 trip_queue = queue.Queue()
+trip_data_queue = queue.Queue()
+
+
+def con() -> psycopg2:
+    conn: psycopg2 = \
+        psycopg2.connect(user='admin', password='', host='127.0.0.1', port='5432', database='misc')
+
+    def clean_up() -> None:
+        conn.close()
+        print('Closing connection', conn)
+
+    atexit.register(clean_up)
+    return conn
 
 
 def get_cookie() -> str:
@@ -95,8 +112,48 @@ def get_trip_details() -> None:
             data = {"tripUUID": uuid}
             resp = requests.post(url, json=data, headers=get_headers()).text
             print(resp)
+            json_resp = json.loads(resp)
+            trip_data_queue.put_nowait(json_resp)
+        except json.decoder.JSONDecodeError:
+            print('Bad JSON decode')
+            continue
         except queue.Empty:
             print('Finished trip queue')
+            return
+
+
+def get_index_price(t, n: int) -> Optional[float]:
+    try:
+        float(t['breakdown'][0]['items'][n]['amount'])
+    except:
+        return None
+
+
+def insert_trip_sql() -> None:
+    conn = con()
+    insert_sql: str = 'INSERT INTO misc.public.trips (uuid,vehicle_type, total, request_at, is_surge, distance, duration, pickup_address, dropoff_address, status, total_toll, custom_route_map, chain_uuid, driver_fare, dropoff_at, distance_price, duration_price, surge_price) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) On CONFLICT DO NOTHING '
+
+    while True:
+        try:
+            trip_obj = trip_data_queue.get()
+            t = trip_obj['data']['tripDetails']
+            total_toll: Optional[float] = None if t['totalToll'] is None else float(t['totalToll'])
+            dropoff_at: Optional[datetime] = None if t['dropoffAt'] is None else datetime.fromtimestamp(t['dropoffAt'])
+
+            data = [
+                t['uuid'], t['vehicleType'], float(t['total']), datetime.fromtimestamp(t['requestAt']),
+                t['isSurge'], float(t['distance']), int(t['duration']), t['pickupAddress'], t['dropoffAddress'],
+                t['status'], total_toll, t['customRouteMap'], t['chainUuid'], float(t['driverFare']),dropoff_at,
+                get_index_price(t, 0), get_index_price(t, 1), get_index_price(t, 2)
+            ]
+            print(data)
+            cursor = conn.cursor()
+            cursor.execute(insert_sql, data)
+
+            conn.commit()
+            cursor.close()
+        except queue.Empty:
+            print('Done inserting trip data')
             return
 
 
@@ -112,6 +169,8 @@ def main() -> None:
     threading.Thread(target=get_trip_details).start()
     threading.Thread(target=get_trip_details).start()
     threading.Thread(target=get_trip_details).start()
+
+    insert_trip_sql()
 
 
 if __name__ == '__main__':
